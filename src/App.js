822117b94ddd315080f1730ceed52ce6d658f06e
abrar zahin <abrarzahin@yahoo.com>
2019-07-23 22:07:02 -0400

    4 post showing in the left content of the grid

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import './App.css';
+import MuiThemeProvider from '@material-ui/core/styles/MuiThemeProvider';
+import createMuiTheme from '@material-ui/core/styles/createMuiTheme';
 
 import Navbar from './components/Navbar';
 
@@ -8,20 +10,42 @@ import home from './pages/home';
 import login from './pages/login';
 import signup from './pages/signup';
 
+const theme = createMuiTheme({
+  palette: {
+    primary: {
+      light: '#487e4c',
+      main: '#1b5e20',
+      dark: '#124116',
+      contrastText: '#fff'
+    },
+    secondary: {
+      light: '#ff6333',
+      main: '#ff3d00',
+      dark: '#b22a00',
+      contrastText: '#fff'
+    }
+  },
+  typography: {
+    useNextVariants: true
+  }
+});
+
 function App() {
   return (
+    <MuiThemeProvider theme={theme}>
     <div className="App">
       <Router>
-      <Navbar />
-          <div className="container">
-            <Switch>
-              <Route exact path="/" component={home} />
-              <Route exact path="/login" component={login} />
-              <Route exact path="/signup" component={signup} />
-            </Switch>
-            </div>
-        </Router>
+        <Navbar />
+        <div className="container">
+          <Switch>
+            <Route exact path="/" component={home} />
+            <Route exact path="/login" component={login} />
+            <Route exact path="/signup" component={signup} />
+          </Switch>
+        </div>
+      </Router>
     </div>
+  </MuiThemeProvider>
   );
 }
 
