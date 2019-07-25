00d52923c5d7404d280012c9badc97d33be7e377
abrar zahin <abrarzahin@yahoo.com>
2019-07-25 00:14:19 -0400

    7 submit button,functionality and errors- login-2

--- src/pages/login.js
+++ src/pages/login.js
@@ -2,11 +2,15 @@ import React, { Component } from 'react';
 import withStyles from '@material-ui/core/styles/withStyles';
 import PropTypes from 'prop-types';
 import AppIcon from '../images/fgicon.png';
+import axios from 'axios';
+import { Link } from 'react-router-dom';
 
 
 import Grid from '@material-ui/core/Grid';
 import Typography from '@material-ui/core/Typography';
 import TextField from '@material-ui/core/TextField';
+import Button from '@material-ui/core/Button';
+import CircularProgress from '@material-ui/core/CircularProgress';
 
 const styles = {
     form: {
@@ -17,6 +21,27 @@ const styles = {
       },
       pageTitle: {
         margin: '10px auto 10px auto'
+      },
+      image: {
+        margin: '20px auto 20px auto'
+      },
+      pageTitle: {
+        margin: '10px auto 10px auto'
+      },
+      textField: {
+        margin: '10px auto 10px auto'
+      },
+      button: {
+        marginTop: 20,
+        position: 'relative'
+      },
+      customError: {
+        color: 'red',
+        fontSize: '0.8rem',
+        marginTop: 10
+      },
+      progress: {
+        position: 'absolute'
       }
  
   };
@@ -34,7 +59,29 @@ class login extends Component {
         };
       }
       handleSubmit = (event) => {
-        console.log('submit works')
+        event.preventDefault();
+        this.setState({
+          loading: true
+        });
+        const userData = {
+          email: this.state.email,
+          password: this.state.password
+        };
+        axios
+          .post('/login', userData)
+          .then((res) => {
+            console.log(res.data);
+            this.setState({
+              loading: false
+            });
+            this.props.history.push('/');
+          })
+          .catch((err) => {
+            this.setState({
+              errors: err.response.data,
+              loading: false
+            });
+          });
       };
       handleChange = (event) => {
         this.setState({
@@ -50,7 +97,7 @@ class login extends Component {
             <Grid item sm />
             <Grid item sm>
               <img src={AppIcon} alt="social-media" className={classes.image} />
-              <Typography variant="h2" className={classes.pageTitle}>
+              <Typography variant="h3" className={classes.pageTitle}>
                 Login
               </Typography>
               <form noValidate onSubmit={this.handleSubmit}>
@@ -77,10 +124,32 @@ class login extends Component {
               value={this.state.password}
               onChange={this.handleChange}
               fullWidth
-            />
+               />
+               {errors.general && (
+              <Typography variant="body2" className={classes.customError}>
+                {errors.general}
+              </Typography>
+               )}
 
+              <Button
+              type="submit"
+              variant="contained"
+              color="primary"
+              className={classes.button}
+              disabled={loading}
+            >
+               Login
+            {loading && (
+                <CircularProgress size={30} className={classes.progress} />
+            )}
+             
+            </Button>
+            <br />
+            <small>
+              dont have an account ? sign up <Link to="/signup">here</Link>
+            </small>
 
-              </form>
+             </form>
               
             </Grid>
             <Grid item sm />
