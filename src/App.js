915ea1b9dd8029e44a65e5ff24df81b67821a0fe
abrar zahin <abrarzahin@yahoo.com>
2019-07-25 21:50:00 -0400

    8 signup page

--- src/App.js
+++ src/App.js
@@ -5,30 +5,13 @@ import MuiThemeProvider from '@material-ui/core/styles/MuiThemeProvider';
 import createMuiTheme from '@material-ui/core/styles/createMuiTheme';
 
 import Navbar from './components/Navbar';
+import themeObject from './util/theme';
 
 import home from './pages/home';
 import login from './pages/login';
 import signup from './pages/signup';
 
-const theme = createMuiTheme({
-  palette: {
-    primary: {
-      light: '#33c9dc',
-      main: '#00bcd4',
-      dark: '#008394',
-      contrastText: '#fff'
-    },
-    secondary: {
-      light: '#ff6333',
-      main: '#ff3d00',
-      dark: '#b22a00',
-      contrastText: '#fff'
-    }
-  },
-  typography: {
-    useNextVariants: true
-  }
-});
+const theme = createMuiTheme(themeObject);
 
 function App() {
   return (
