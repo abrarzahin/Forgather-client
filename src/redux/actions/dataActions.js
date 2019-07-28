4416dbcfc1c1a2d4a9688897106cf4d3ddd9189f
abrar zahin <abrarzahin@yahoo.com>
2019-07-28 17:58:45 -0400

    35 comments submission on post

--- src/redux/actions/dataActions.js
+++ src/redux/actions/dataActions.js
@@ -9,7 +9,8 @@ import {
     CLEAR_ERRORS,
     LOADING_UI,
     SET_SCREAM,
-    STOP_LOADING_UI
+    STOP_LOADING_UI,
+    SUBMIT_COMMENT
   } from '../types';
   import axios from 'axios';
   
@@ -75,7 +76,7 @@ import {
           type: POST_SCREAM,
           payload: res.data
         });
-        dispatch({ type: CLEAR_ERRORS });
+        dispatch(clearErrors());
       })
       .catch((err) => {
         dispatch({
@@ -97,6 +98,24 @@ import {
         dispatch({ type: STOP_LOADING_UI });
       })
       .catch((err) => console.log(err));
+  };
+
+  export const submitComment = (screamId, commentData) => (dispatch) => {
+    axios
+      .post(`/scream/${screamId}/comment`, commentData)
+      .then((res) => {
+        dispatch({
+          type: SUBMIT_COMMENT,
+          payload: res.data
+        });
+        dispatch(clearErrors());
+      })
+      .catch((err) => {
+        dispatch({
+          type: SET_ERRORS,
+          payload: err.response.data
+        });
+      });
   };
 
   export const clearErrors = () => (dispatch) => {
